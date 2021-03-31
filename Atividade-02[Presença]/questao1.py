e20ddb66ed64019ddbd6ff3edc9d9b7898754fc8
KelvinLR <61692363+KelvinLR@users.noreply.github.com>
2021-03-31 15:59:17 -0300

    Delete Atividade-02[Presença] directory

--- Atividade-02[Presença]/questao1.py
+++
@@ -1,8 +0,0 @@
-listaExercicio = [2, 4, 5, 7, 9]
-
-listaExercicio.insert(0, 13)
-print("Adicionando elemento na pilha: ", listaExercicio)
-
-listaExercicio.pop(0)
-print("Removendo elemento na pilha: ", listaExercicio)
-
--- Atividade-02[Presença]/questao3.py
+++
@@ -1,7 +0,0 @@
-listaExercicio = [2, 4, 5, 7, 9]
-
-listaExercicio.insert(2, 13)
-print("Adicionando elemento na lista encadeada: ", listaExercicio)
-
-listaExercicio.pop(1)
-print("Remover elemento na lista encadeada: ", listaExercicio)
